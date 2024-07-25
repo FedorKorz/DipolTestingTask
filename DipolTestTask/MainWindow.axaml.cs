1e62913fdf925bee4473f61159cf2b324ba67a52
FedorKorz <fedorkorz@gmail.com>
2024-07-25 15:49:13 +0300

    Правки, новый функционал

--- DipolTestTask/MainWindow.axaml.cs
+++ DipolTestTask/MainWindow.axaml.cs
@@ -1,22 +1,40 @@
-using System;
 using Avalonia.Controls;
 using Avalonia.Media;
+using Avalonia.Interactivity;
+using static DipolTestTask.Utils.Utils;
 
 namespace DipolTestTask;
 
 public partial class MainWindow : Window
 {
+    private readonly TextBox? _textBox;
+    private readonly TextBlock? _errorTextBlock;
+
     public MainWindow()
     {
         InitializeComponent();
         var button = this.FindControl<Button>("MyButton");
-        button.Click += OnButtonClick;
+        _textBox = this.FindControl<TextBox>("MyTextBox");
+        _errorTextBlock = this.FindControl<TextBlock>("ErrorTextBlock");
+
+        if (button != null) button.Click += OnButtonClick;
     }
-    
-    private void OnButtonClick(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+
+    private void OnButtonClick(object sender, RoutedEventArgs e)
     {
-        var textBox = this.FindControl<TextBox>("MyTextBox");
-        textBox.Text = "Button clicked!";
-        textBox.Background = new SolidColorBrush(Colors.Red); // Изменить цвет фона
+        var colorFromTextBox = _textBox?.Text;
+
+        if (IsValidColorCode(colorFromTextBox))
+            try
+            {
+                _textBox.Background = new SolidColorBrush(Color.Parse(colorFromTextBox));
+                _errorTextBlock.Text = string.Empty;
+            }
+            catch
+            {
+                _errorTextBlock.Text = "Invalid color code.";
+            }
+        else
+            _errorTextBlock.Text = "Неверный цветовой код. Используйте #RRGGBB формат.";
     }
 }
---
+++ DipolTestTask/Utils/Utils.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DipolTestTask.Utils;
+
+public static class Utils
+{
+    public static bool IsValidColorCode(string? colorCode)
+    {
+        return !string.IsNullOrEmpty(colorCode) && Regex.IsMatch(colorCode, "^#[0-9A-Fa-f]{6}$");
+    }
+}
